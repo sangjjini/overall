7cf92f60263f5eb3183e7bc81de94b1fe5968288
sangjini <esj7675@naver.com>
2023-08-10 18:01:47 +0900

    Member dao,Vo.. commit

---
+++ src/main/java/com/example/spring_project/service/MatchService.java
@@ -0,0 +1,28 @@
+package com.example.spring_project.service;
+
+import com.example.spring_project.domain.match.Match;
+import com.example.spring_project.domain.match.MatchRepository;
+import com.example.spring_project.domain.match.MatchRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MatchService {
+    private final MatchRepository matchRepository;
+
+    @Transactional
+    public void updateMatch(int no, MatchRequestDto dto){
+        Match match = matchRepository.findById(no).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 경기입니다.")
+        );
+        match.update(dto);
+    }
+
+    @Transactional
+    public void deleteMatch(int no){
+        matchRepository.deleteById(no);
+    }
+}
