efea90c98992ef9ffcc34b16054d54aaecaa67c3
alstmd7 <qortmdals120@gmail.com>
2023-08-22 10:10:35 +0900

    #3-BackSeungmin Modified delete making

--- src/main/java/com/example/spring_project/domain/squad/SquadRepository.java
+++ src/main/java/com/example/spring_project/domain/squad/SquadRepository.java
@@ -3,8 +3,6 @@ package com.example.spring_project.domain.squad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SquadRepository extends JpaRepository<Squad, Long> {
-
-    Squad findByMaking(String email);
     Squad findByNo(Long no);
     Squad findByHost(String author);
     Squad findByName(String name);
